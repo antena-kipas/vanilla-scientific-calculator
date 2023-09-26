20aec5028c8a273faeaca0b89af44cfbb6aead6a
antena-kipas <ujankcodet0@gmail.com>
2023-09-26 12:05:20 +0700

    26 sep 2023 oleh hasan

---
+++ vanilla-scientific-calculator.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+landasan_gui = tk.Tk()
+landasan_gui.configure(bg="white")
+landasan_gui.geometry("420x560")
+landasan_gui.resizable(False,False)
+landasan_gui.title("Vanilla Scientific Caculator")
+
+
+mainloop()
